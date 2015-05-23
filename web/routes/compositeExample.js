d7e2c4bdf8a726abd23bea17b207a45992fbf907
Aaron Saunders <aaron@clearlyinnovative.com>
2015-05-22 23:57:19 -0400

    using API route to create the models to support a composite model

--- web/routes/compositeExample.js
+++ web/routes/compositeExample.js
@@ -1,18 +1,44 @@
 var Arrow = require('arrow');
-var ArrowBuilder = require('arrowBuilder');
 
 var TestRoute = Arrow.Router.extend({
-	name: 'compositeExample',
-	path: '/compositeExample',
-	method: 'GET',
-	description: 'this is an example web route, accessing composite model data',
-	action: function (req, resp) {
-		/**
-		 * by default, routes are sync. to make them async, add a next in the action above as
-		 * the last parameter and then call next when completed
-		 */
-		resp.render('example');
-	}
+    name : 'compositeExample',
+    path : '/compositeExample',
+    method : 'POST',
+    description : 'this is an example web route, accessing composite model data',
+    action : function(req, resp, next) {
+        var homeParams = {
+            "name" : "Test Name - Vacation Home",
+            "address" : "No Address",
+            "comment" : "Sample test"
+        };
+        var FinalHomeModel = Arrow.Model.getModel('finalHome');
+        var FinalRoomModel = Arrow.Model.getModel('finalRoom');
+
+        FinalHomeModel.create(homeParams, function(err, results) {
+            if (err) {
+                req.logger.error(JSON.stringify(err));
+                next(err);
+            } else {
+                req.logger.info('got FinalHomeModel ' + JSON.stringify(results));
+
+                var roomParams = {
+                    "home_id" : results.id,
+                    "name" : "Entertainment Room - Vacation Home",
+                    "comment" : "Sample test"
+                };
+                FinalRoomModel.create(roomParams, function(err, results) {
+                    if (err) {
+                        req.logger.error(JSON.stringify(err));
+                        next(err);
+                    } else {
+                        req.logger.info('got FinalRoomModel ' + JSON.stringify(results));
+                        resp.json(results);
+                        next(null);
+                    }
+                });
+            }
+        });
+    }
 });
 
 module.exports = TestRoute;
